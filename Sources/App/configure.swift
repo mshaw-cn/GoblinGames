caeab603d79b9aef593a24eea9995ad714a60f4c
Michael Shaw <mshaw.cn@icloud.com>
2021-11-26 13:57:06 -0600

    implement basic styling structure

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,6 +8,7 @@ public func configure(_ app: Application) throws {
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
     app.views.use(.leaf)
+    
     // register routes
     try routes(app)
 }
